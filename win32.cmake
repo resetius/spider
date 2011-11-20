c14b4e72863bdf9a31e75502117f0efd473fc910
Alexey Ozeritsky <aozeritsky@gmail.com>
2011-11-20 15:57:30 +0400

    win32 fixes

--- win32.cmake
+++ win32.cmake
@@ -1,5 +1,6 @@
 add_definitions(-D_CRT_SECURE_NO_WARNINGS)
 set(BOOST_ROOT "c:/Build/boost_1_48_0")
+# b2 architecture=x86 address-model=64 link=static runtime-link=shared
 set(Boost_USE_STATIC_LIBS TRUE)
 set(Boost_ADDITIONAL_VERSIONS "1.48" "1.48.0")
 set(Berkeley_ROOT "c:/Build/db-5.2.36")
