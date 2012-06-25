7bfdb41e7a70801e9765962fc1811ca91eb72e43
Alexey Ozeritsky <aozeritsky@gmail.com>
2012-06-25 18:12:27 +0400

    unit tests

--- test_links_db.cpp
+++ test_links_db.cpp
@@ -69,7 +69,60 @@ void test_flags()
 		exit(1);
 	}	
 	if (ldb.is_downloading(id1) == true) {
+		fprintf(stderr, "failed (5)");
+		exit(1);
+	}
+}
+
+void test_converters()
+{
+	boost::filesystem::remove_all(path);
+
+	LinksDb ldb(path);
+
+	uint64_t id1 = ldb.id("url1");
+	uint64_t id2 = ldb.id("url2");
+
+	if (ldb.url(id1) != "url1") {
+		fprintf(stderr, "failed (1)");
+		exit(1);
+	}		
+	if (ldb.url(id2) != "url2") {
 		fprintf(stderr, "failed (2)");
+		exit(1);
+	}
+}
+
+void test_restore()
+{
+	boost::filesystem::remove_all(path);
+
+	LinksDb ldb(path);
+
+	uint64_t id1 = ldb.id("url1");
+	uint64_t id2 = ldb.id("url2");
+
+	ldb.mark_downloading(id1);
+	ldb.mark_downloading(id2);
+
+	ldb.restore_downloading();
+
+	uint64_t id3 = ldb.pop_link();
+	uint64_t id4 = ldb.pop_link();
+	uint64_t id5 = ldb.pop_link();
+
+	if (id5 != -1) {
+		fprintf(stderr, "failed (1)");
+		exit(1);
+	}
+
+	if (id3 != id1) {
+		fprintf(stderr, "failed (2)");
+		exit(1);
+	}
+
+	if (id4 != id2) {
+		fprintf(stderr, "failed (3)");
 		exit(1);
 	}
 }
@@ -85,6 +138,10 @@ int main(int argc, char ** argv)
 		test_queue();
 	} else if (!strcmp(argv[1], "flags")) {
 		test_flags();
+	} else if (!strcmp(argv[1], "converters")) {
+		test_converters();
+	} else if (!strcmp(argv[1], "restore")) {
+		test_restore();
 	}
 
 	mylog_close();
